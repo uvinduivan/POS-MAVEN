9f8c05e4c581cbd51e558b5c15bd2856caac5004
uvinduivan <uvinduivan456@gmail.com>
2024-01-06 13:28:47 +0530

    Bo updated (generics removed)

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -52,7 +52,7 @@ public class CustomerFormController {
     @FXML
     private TextField txtSalary;
 
-    private CustomerBo<CustomerDto> customerBo = new CustomerBoImpl();
+    private CustomerBo customerBo = new CustomerBoImpl();
 
     public void initialize(){
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
